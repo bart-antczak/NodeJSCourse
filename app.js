f3973e62b2fc9dedb2c486bd0e45d4b8132d238b
bart-mant-io <antczak.bar@gmail.com>
2019-03-22 12:49:53 +0200

    Stylowanie, dodanie path i 404.html

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
-app.use('/shop', shopRoutes);
+app.use(shopRoutes);
 
 app.use((req, res, next) => {
     // Dodanie statusów requestów
