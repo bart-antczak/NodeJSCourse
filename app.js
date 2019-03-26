f65e9fac00fd15e5aea0678e43d84031268e7cdf
bart-mant-io <antczak.bar@gmail.com>
2019-03-26 11:55:34 +0100

    Dodanie CSRF protection

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const csrf = require('csurf');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -17,6 +18,8 @@ const store = new MongoDBStore({
   collection: 'sessions'
 });
 
+const csrfProtection = csrf();
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -29,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(
     session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store })
 );
+app.use(csrfProtection);
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -40,6 +44,12 @@ app.use((req, res, next) => {
         next();
       })
       .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
 });
 
 app.use('/admin', adminRoutes);
