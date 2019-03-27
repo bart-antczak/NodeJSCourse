55b756e41cf706f36b41d07ff3770d1cf8507670
bart-mant-io <antczak.bar@gmail.com>
2019-03-27 11:38:31 +0100

    Authentication module

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 
+const flash = require('connect-flash');
+
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
@@ -33,6 +35,7 @@ app.use(
     session({ secret: 'my secret', resave: false, saveUninitialized: false, store: store })
 );
 app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
   if (!req.session.user) {
